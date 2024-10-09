b34be85d0726b31bbcac7587b0c67d3377092d21
atharvftw <139485305+atharvftw@users.noreply.github.com>
2024-10-09 22:25:33 +0530

    Update Scrollanimation.tsx

--- src/components/Scrollanimation.tsx
+++ src/components/Scrollanimation.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import guiimg from '../assets/gui.jpg';
 
 export const NulifyInterface = () => {
