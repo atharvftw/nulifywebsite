261c683322f2db8cb8aed43f7df470137e25242b
atharvftw <139485305+atharvftw@users.noreply.github.com>
2024-10-10 08:17:49 +0530

    Update Newsletter.tsx

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 import { Check } from 'lucide-react';
 
 export default function ContactForm() {
